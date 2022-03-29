efb75e51b8efe90a2e05d8366028844f70092c9a
forgetsmd <forgetsmd@gmail.com>
2022-03-29 20:14:57 +0300

    fff

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,10 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('watchjs', function() {
+    gulp.watch("src/js/*.js").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('watch','watchjs','server', 'styles'));
